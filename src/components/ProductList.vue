86774ec2a24f6a31d3f8e65311a637733d6359a5
Jens Fischer <soth@gmx.net>
2018-09-07 08:53:56 +0200

    update salesPrice using PATCH

--- src/components/ProductList.vue
+++ src/components/ProductList.vue
@@ -11,9 +11,9 @@
     <div>
       <ol class="products">
         <li v-for="product in products" v-bind:key="product._id">
-          <span>{{ product.name }} </span>
-          <span>{{product.salesPrice.amount}}{{product.salesPrice.currency}} </span>
-          <span>{{ product.listPrice ? '(' + product.listPrice.amount + product.listPrice.currency + ')' : '' }} </span>
+          <span class="product name">{{ product.name }} </span>&nbsp;
+          <span class="product amount">{{ product.salesPrice.amount | amount }}</span><span class="product currency" v-html="currency(product.salesPrice.currency)"></span>&nbsp;
+          <span class="product amount">{{ product.listPrice ? product.listPrice.amount : '' | amount }}</span><span class="product currency" v-html="currency(product.salesPrice.currency)"></span>&nbsp;
           <button v-on:click="reduceSalesPrice(product, 0.15)">On Sale</button>
         </li>
       </ol>
@@ -26,6 +26,7 @@
 import StorageMixin from "../mixins/StorageMixin";
 import axios from "axios";
 import _ from "lodash";
+import numeral from "numeral";
 
 export default {
   mixins: [StorageMixin],
@@ -41,7 +42,24 @@ export default {
     this.fetchProducts();
   },
 
+  filters: {
+    amount: function(value) {
+      return numeral(value).format("0,0.00");
+    }
+  },
+
   methods: {
+    currency: function(value) {
+      switch (value) {
+        case "GBP":
+          return "&pound";
+        case "EUR":
+          return "&euro;";
+        default:
+          return "???";
+      }
+    },
+
     fetchProducts: function() {
       if (this.tokenExpired) {
         this.$router.push("/");
@@ -54,8 +72,7 @@ export default {
           method: "GET",
           url: "/products",
           headers: {
-            "Content-Type": "application/json",
-            Authorization: "Bearer " + this.access_token.bearer
+            Authorization: `Bearer ${this.access_token.bearer}`
           }
         })
         .then(response => {
@@ -72,24 +89,48 @@ export default {
     },
 
     reduceSalesPrice: function(product, reduction) {
-      var reducedPrice = product.salesPrice;
-      reducedPrice.amount -=
-        _.get(product, "listPrice.amount", reducedPrice.amount) * reduction;
+      var reduced = product.salesPrice;
+      reduced.amount -=
+        _.get(product, "listPrice.amount", reduced.amount) * reduction;
 
       var patch = [
-        { op: "add", path: "/tags/-", value: "sale" },
+        { op: "add", path: "/tags/-", value: "Sale" },
         {
           op: "replace",
           path: "/salesPrice",
-          value: reducedPrice
+          value: reduced
         }
       ];
+
+      axios
+        .request({
+          baseURL: this.api_url,
+          timeout: 5000,
+          method: "PATCH",
+          url: `/products/${product._id}`,
+          headers: {
+            "Content-Type": "application/json-patch+json",
+            Authorization: `Bearer ${this.access_token.bearer}`
+          },
+          data: patch
+        })
+        .then(response => {
+          if (response.status === 200) {
+            console.log(response.data);
+          } else {
+            this.errors.push({ message: request.statusText });
+          }
+        })
+        .catch(e => {
+          console.error(e);
+          this.errors.push({ message: "error processing request" });
+        });
       /*
 $ curl 'https://yourshop.api.urn/products/f925d108-379c-4923-90ab-b67563f6ba38?locale=en-GB' -i -X PATCH -H 'Content-Type: application/json-patch+json' -H 'Accept: application/hal+json' -H 'Authorization: Bearer <Access token>' 
 -d '[{"op":"add","path":"/tags/-","value":"sale"},{"op":"replace","path":"/salesPrice","value":{"taxModel":"NET","amount":9.0,"currency":"EUR"}}]'
 */
 
-      return reducedPrice;
+      //return reduced;
     }
   }
 };
